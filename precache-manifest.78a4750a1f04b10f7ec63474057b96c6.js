55ecef4d54a3f947ed78f91021b3f147f6a5221a
UmerF2000 <64559800+UmerF2000@users.noreply.github.com>
2020-09-20 22:56:46 -0500

    Updates

---
+++ precache-manifest.78a4750a1f04b10f7ec63474057b96c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6f4385e96486d8e08cbf9053f5bd03",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3c2c98339aacef15f9f8",
+    "url": "/robofriends/static/css/main.9634a040.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2c98339aacef15f9f8",
+    "url": "/robofriends/static/js/main.db1d3df1.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
